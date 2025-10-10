64ea576c5a2d361c8c3aa532e28fc6ff7886bda4
Lliam Symonds <lliamsymonds04@gmail.com>
2025-10-10 14:56:30 +1000

    added __main__ to train

--- recognition/prostate-cancer-segmenter-s4802711/train.py
+++ recognition/prostate-cancer-segmenter-s4802711/train.py
@@ -2,10 +2,12 @@
 Training script, where training, validation, testing and saving are done
 """
 
+import os
 import torch.optim as optim
 import torch
 import torch.nn.functional as F
 from modules import U2Net
+from dataset import SegmentationDataset, load_data_helper
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -13,5 +15,17 @@ def create_model():
     model = U2Net(in_ch=1, out_ch=4).to(device)
     return model
 
-def train_model(model: U2Net):
-    pass  # Training logic to be implemented here
+def train_model(model: U2Net, train_loader: SegmentationDataset, num_epochs=10):
+    pass
+
+
+if __name__ == "__main__":
+    model = create_model()
+
+    # load data
+    base_dir = "recognition/prostate-cancer-segmenter-s4802711/keras_slices_data"
+    base_dir = os.path.abspath(base_dir)
+    dataset = load_data_helper(base_dir, "train")
+    
+
+    train_model(model, dataset, num_epochs=50)
