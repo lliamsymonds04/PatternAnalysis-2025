7754654c9cffdf19465c59707c7d12e26c109212
Lliam Symonds <lliamsymonds04@gmail.com>
2025-10-10 14:52:30 +1000

    added model creation in train_model

--- recognition/prostate-cancer-segmenter-s4802711/train.py
+++ recognition/prostate-cancer-segmenter-s4802711/train.py
@@ -1,3 +1,17 @@
 """
 Training script, where training, validation, testing and saving are done
 """
+
+import torch.optim as optim
+import torch
+import torch.nn.functional as F
+from modules import U2Net
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def create_model():
+    model = U2Net(in_ch=1, out_ch=4).to(device)
+    return model
+
+def train_model(model: U2Net):
+    pass  # Training logic to be implemented here
