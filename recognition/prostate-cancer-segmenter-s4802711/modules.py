2617f9f3617e812b4a2439aa011e97f995263ac6
Lliam Symonds <lliamsymonds04@gmail.com>
2025-10-10 14:43:12 +1000

    added encoder decoder to rsu block

--- recognition/prostate-cancer-segmenter-s4802711/modules.py
+++ recognition/prostate-cancer-segmenter-s4802711/modules.py
@@ -5,7 +5,35 @@ Components for the 2D U-Net architecture.
 import torch.nn as nn
 import torch.nn.functional as F
 
-class RSU(nn.m):
-    def __init__(self, in_ch, out_ch, mid_ch, depth):
+class RSU(nn.Module):
+    def __init__(self, in_ch: int, out_ch: int, mid_ch: int, depth: int = 7):
         super(RSU, self).__init__()
         self.depth = depth
+
+        self.rebnconvin = self._make_conv_block(in_ch, out_ch)
+
+        # encoder stages
+        self.rebnconv1 = self._make_conv_block(out_ch, mid_ch)
+        self.pool = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        self.rebconvs = nn.ModuleList()
+        for i in range(2, depth):
+            self.rebconvs.append(self._make_conv_block(mid_ch, mid_ch))
+
+        self.rebconvm = self._make_conv_block(mid_ch, mid_ch)
+
+        # decoder stages
+        self.rebconvd = nn.ModuleList()
+        for i in range(2, depth):
+            self.rebconvd.append(self._make_conv_block(mid_ch * 2, mid_ch))
+
+        self.rebnconvout = self._make_conv_block(mid_ch * 2, out_ch)
+
+    def _make_conv_block(self, in_ch: int, out_ch: int):
+        return nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return x
