90d0c486966d50e183e1ae91a636eebf4491ea8d
Lliam Symonds <lliamsymonds04@gmail.com>
2025-10-10 14:33:25 +1000

    added skeleton for rsu code

--- recognition/prostate-cancer-segmenter-s4802711/modules.py
+++ recognition/prostate-cancer-segmenter-s4802711/modules.py
@@ -1,3 +1,11 @@
 """
 Components for the 2D U-Net architecture.
 """
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class RSU(nn.m):
+    def __init__(self, in_ch, out_ch, mid_ch, depth):
+        super(RSU, self).__init__()
+        self.depth = depth
